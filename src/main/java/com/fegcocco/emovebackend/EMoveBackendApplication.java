640252fcd16762bcf4b685b68bb8ef83fdea96ba
FeCocco <felipegco@proton.me>
2025-08-31 17:31:08 -0300

    Primeiro commit

---
+++ src/main/java/com/fegcocco/emovebackend/EMoveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fegcocco.emovebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMoveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMoveBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/fegcocco/emovebackend/Entity/Usuario.java
@@ -0,0 +1,40 @@
+package com.fegcocco.emovebackend.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "USUARIOS")
+@Getter
+@Setter
+public class Usuario {
+
+    @Enumerated(EnumType.STRING)
+    private Sexo sexo;
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String cpf;
+
+    private Date dataNascimento;
+
+    public enum Sexo {
+        MASCULINO,
+        FEMININO
+    }
+
+    private int Telefone;
+
+    private String email;
+
+    private String senha;
+
+    private Date dataCadastro;
+
+}
