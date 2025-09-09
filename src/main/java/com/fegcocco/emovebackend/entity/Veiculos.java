a59306ce40147e707c9138cdb0aa66af638a6d95
FeCocco <felipegco@proton.me>
2025-09-09 10:17:25 -0300

    Feat: Adiconado as principais colunas da tabela veículos

--- src/main/java/com/fegcocco/emovebackend/entity/Veiculos.java
+++ src/main/java/com/fegcocco/emovebackend/entity/Veiculos.java
@@ -12,6 +12,28 @@ public class Veiculos {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private Long id;
 
+    @Column(nullable = false)
+    private String marca;
+
+    @Column(nullable = false)
+    private String modelo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "TIPO_PLUGIN", nullable = false, length = 20)
+    private TipoPlugin tipoPlugin;
+
+    public enum TipoPlugin {
+        SAE_TIPO_1,
+        TIPO_2_AC,
+        CCS_TIPO_2,
+        CHADEMO,
+        GBT
+    }
+
+    @Column(nullable = false)
+    private int autonomia;
 }
